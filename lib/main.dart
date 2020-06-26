79c8c64ae95a046381500b126fa2b8be277c1039
Himanshu Jain <himanshu12221@gmail.com>
2020-06-26 21:48:58 +0530

    Multilevel dropdown for states and countries for India

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/services.dart';
-import 'homepage.dart';
+import "repository.dart";
 
 void main() => runApp(MyApp());
 
@@ -9,185 +7,86 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Phone Authentication',
-      routes: <String, WidgetBuilder>{
-        '/homepage': (BuildContext context) => MyHome(),
-        '/loginpage': (BuildContext context) => MyApp(),
-      },
+      title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyAppPage(title: 'Phone Authentication'),
+      home: Home(),
     );
   }
 }
 
-class MyAppPage extends StatefulWidget {
-  MyAppPage({Key key, this.title}) : super(key: key);
-  final String title;
-
+class Home extends StatefulWidget {
   @override
-  _MyAppPageState createState() => _MyAppPageState();
+  _HomeState createState() => _HomeState();
 }
 
-class _MyAppPageState extends State<MyAppPage> {
-  String phoneNo;
-  String smsOTP;
-  String verificationId;
-  String errorMessage = '';
-  FirebaseAuth _auth = FirebaseAuth.instance;
+class _HomeState extends State<Home> {
+  Repository repo = Repository();
 
-  Future<void> verifyPhone() async {
-    final PhoneCodeSent smsOTPSent = (String verId, [int forceCodeResend]) {
-      this.verificationId = verId;
-      smsOTPDialog(context).then((value) {
-        print('sign in');
-      });
-    };
-    try {
-      await _auth.verifyPhoneNumber(
-          phoneNumber: this.phoneNo, // PHONE NUMBER TO SEND OTP
-          codeAutoRetrievalTimeout: (String verId) {
-            //Starts the phone number verification process for the given phone number.
-            //Either sends an SMS with a 6 digit code to the phone number specified, or sign's the user in and [verificationCompleted] is called.
-            this.verificationId = verId;
-          },
-          codeSent:
-              smsOTPSent, // WHEN CODE SENT THEN WE OPEN DIALOG TO ENTER OTP.
-          timeout: const Duration(seconds: 20),
-          verificationCompleted: (AuthCredential phoneAuthCredential) {
-            print(phoneAuthCredential);
-          },
-          verificationFailed: (AuthException exceptio) {
-            print('${exceptio.message}');
-          });
-    } catch (e) {
-      handleError(e);
-    }
-  }
+  List<String> _states = ["Choose a state"];
+  List<String> _districts = ["Choose .."];
+  String _selectedState = "Choose a state";
+  String _selectedLGA = "Choose ..";
 
-  Future<bool> smsOTPDialog(BuildContext context) {
-    return showDialog(
-        context: context,
-        barrierDismissible: false,
-        builder: (BuildContext context) {
-          return new AlertDialog(
-            title: Text('Enter SMS Code'),
-            content: Container(
-              height: 85,
-              child: Column(children: [
-                TextField(
-                  onChanged: (value) {
-                    this.smsOTP = value;
-                  },
-                ),
-                (errorMessage != ''
-                    ? Text(
-                        errorMessage,
-                        style: TextStyle(color: Colors.red),
-                      )
-                    : Container())
-              ]),
-            ),
-            contentPadding: EdgeInsets.all(10),
-            actions: <Widget>[
-              FlatButton(
-                child: Text('Done'),
-                onPressed: () {
-                  _auth.currentUser().then((user) {
-                    if (user != null) {
-                      Navigator.of(context).pop();
-                      Navigator.of(context).pushReplacementNamed('/homepage');
-                    } else {
-                      signIn();
-                    }
-                  });
-                },
-              )
-            ],
-          );
-        });
-  }
-
-  signIn() async {
-    try {
-      final AuthCredential credential = PhoneAuthProvider.getCredential(
-        verificationId: verificationId,
-        smsCode: smsOTP,
-      );
-      //final FirebaseUser user = (await _auth.signInWithCredential(credential)) ;
-      //final FirebaseUser currentUser = await _auth.currentUser();
-      //assert(user.uid == currentUser.uid);
-      Navigator.of(context).pop();
-      Navigator.of(context).pushReplacementNamed('/homepage');
-    } catch (e) {
-      handleError(e);
-    }
-  }
-
-  handleError(PlatformException error) {
-    print(error);
-    switch (error.code) {
-      case 'ERROR_INVALID_VERIFICATION_CODE':
-        FocusScope.of(context).requestFocus(new FocusNode());
-        setState(() {
-          errorMessage = 'Invalid Code';
-        });
-        Navigator.of(context).pop();
-        smsOTPDialog(context).then((value) {
-          print('sign in');
-        });
-        break;
-      default:
-        setState(() {
-          errorMessage = error.message;
-        });
-
-        break;
-    }
+  @override
+  void initState() {
+    _states = List.from(_states)..addAll(repo.getStates());
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text("STATES MULTI DROPDOWN"),
+        elevation: 0.1,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Padding(
-              padding: EdgeInsets.all(10),
-              child: TextField(
-                decoration: InputDecoration(
-                    hintText: 'Enter Phone Number Eg. +910000000000'),
-                onChanged: (value) {
-                  this.phoneNo = value;
-                },
+      body: SafeArea(
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 50, vertical: 30),
+          child: Column(
+            children: <Widget>[
+              DropdownButton<String>(
+                isExpanded: true,
+                items: _states.map((String dropDownStringItem) {
+                  return DropdownMenuItem<String>(
+                    value: dropDownStringItem,
+                    child: Text(dropDownStringItem),
+                  );
+                }).toList(),
+                onChanged: (value) => _onSelectedState(value),
+                value: _selectedState,
               ),
-            ),
-            (errorMessage != ''
-                ? Text(
-                    errorMessage,
-                    style: TextStyle(color: Colors.red),
-                  )
-                : Container()),
-            SizedBox(
-              height: 10,
-            ),
-            RaisedButton(
-              onPressed: () {
-                verifyPhone();
-              },
-              child: Text('Verify'),
-              textColor: Colors.white,
-              elevation: 7,
-              color: Colors.blue,
-            )
-          ],
+              DropdownButton<String>(
+                isExpanded: true,
+                items: _districts.map((String dropDownStringItem) {
+                  return DropdownMenuItem<String>(
+                    value: dropDownStringItem,
+                    child: Text(dropDownStringItem),
+                  );
+                }).toList(),
+                // onChanged: (value) => print(value),
+                onChanged: (value) => _onSelectedLGA(value),
+                value: _selectedLGA,
+              ),
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  void _onSelectedState(String value) {
+    setState(() {
+      _selectedLGA = "Choose ..";
+      _districts = ["Choose .."];
+      _selectedState = value;
+      _districts = List.from(_districts)..addAll(repo.getLocalByState(value));
+    });
+  }
+
+  void _onSelectedLGA(String value) {
+    setState(() => _selectedLGA = value);
   }
 }
